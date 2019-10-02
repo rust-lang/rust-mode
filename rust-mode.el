8afb3c963ee3e576061e4eacb6cf9757ea22b3ba
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2019-10-03 02:07:37 +0300

    Highlight variable name in a for-loop
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

--- rust-mode.el
+++ rust-mode.el
@@ -700,7 +700,7 @@ Returns nil if the point is not within a Rust string."
      (,rust-re-type-or-constructor 1 font-lock-type-face)
 
      ;; Type-inferred binding
-     (,(concat "\\_<\\(?:let\\s-+ref\\|let\\|ref\\)\\s-+\\(?:mut\\s-+\\)?" (rust-re-grab rust-re-ident) "\\_>") 1 font-lock-variable-name-face)
+     (,(concat "\\_<\\(?:let\\s-+ref\\|let\\|ref\\|for\\)\\s-+\\(?:mut\\s-+\\)?" (rust-re-grab rust-re-ident) "\\_>") 1 font-lock-variable-name-face)
 
      ;; Type names like `Foo::`, highlight excluding the ::
      (,(rust-path-font-lock-matcher rust-re-uc-ident) 1 font-lock-type-face)
