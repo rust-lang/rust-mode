a4eaeb2c0e428e31b91d623593e068677949e088
Jonas Bernoulli <jonas@bernoul.li>
2021-03-29 19:09:00 +0200

    Create rust-playpen.el from existing code

--- rust-mode.el
+++ rust-mode.el
@@ -16,7 +16,6 @@
 ;;; Code:
 
 (eval-when-compile (require 'rx))
-(eval-when-compile (require 'url-vars))
 
 (require 'json)
 (require 'thingatpt)
@@ -53,16 +52,6 @@ When nil, `where' will be aligned with `fn' or `trait'."
   :type 'boolean
   :group 'rust-mode
   :safe #'booleanp)
-
-(defcustom rust-playpen-url-format "https://play.rust-lang.org/?code=%s"
-  "Format string to use when submitting code to the playpen."
-  :type 'string
-  :group 'rust-mode)
-
-(defcustom rust-shortener-url-format "https://is.gd/create.php?format=simple&url=%s"
-  "Format string to use for creating the shortened link of a playpen submission."
-  :type 'string
-  :group 'rust-mode)
 
 (defcustom rust-match-angle-brackets t
   "Whether to enable angle bracket (`<' and `>') matching where appropriate."
@@ -1620,37 +1609,6 @@ This is written mainly to be used as `end-of-defun-function' for Rust."
   (rust--compile "%s test" rust-cargo-bin))
 
 ;;; Secondary Commands
-
-(defun rust-playpen-region (begin end)
-  "Create a shareable URL for the region from BEGIN to END on the Rust playpen."
-  (interactive "r")
-  (let* ((data (buffer-substring begin end))
-         (escaped-data (url-hexify-string data))
-         (escaped-playpen-url (url-hexify-string
-                               (format rust-playpen-url-format escaped-data))))
-    (if (> (length escaped-playpen-url) 5000)
-        (error "encoded playpen data exceeds 5000 character limit (length %s)"
-               (length escaped-playpen-url))
-      (let ((shortener-url (format rust-shortener-url-format escaped-playpen-url))
-            (url-request-method "POST"))
-        (url-retrieve shortener-url
-                      (lambda (state)
-                        ;; filter out the headers etc. included at the
-                        ;; start of the buffer: the relevant text
-                        ;; (shortened url or error message) is exactly
-                        ;; the last line.
-                        (goto-char (point-max))
-                        (let ((last-line (thing-at-point 'line t))
-                              (err (plist-get state :error)))
-                          (kill-buffer)
-                          (if err
-                              (error "failed to shorten playpen url: %s" last-line)
-                            (message "%s" last-line)))))))))
-
-(defun rust-playpen-buffer ()
-  "Create a shareable URL for the contents of the buffer on the Rust playpen."
-  (interactive)
-  (rust-playpen-region (point-min) (point-max)))
 
 (defun rust-promote-module-into-dir ()
   "Promote the module file visited by the current buffer into its own directory.
