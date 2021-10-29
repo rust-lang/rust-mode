d2b4cde98b660efd746d8853cf8ac04e4570e047
brotzeit <brotzeitmacher@gmail.com>
2021-10-29 13:33:55 +0200

    bump version to 1.0.1

--- rust-mode.el
+++ rust-mode.el
@@ -1,6 +1,6 @@
 ;;; rust-mode.el --- A major-mode for editing Rust source code -*-lexical-binding: t-*-
 
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Author: Mozilla
 ;; Url: https://github.com/rust-lang/rust-mode
 ;; Keywords: languages
