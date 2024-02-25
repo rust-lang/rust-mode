a73d8266a950413fea56f070065aea6a3076cd1e
Sibi Prabakaran <sibi@psibi.in>
2024-02-25 10:31:22 +0530

    Fix rust-mode and tree sitter integration

---
+++ rust-common.el
@@ -0,0 +1,18 @@
+;;; rust-common.el --- Common code for both modes -*-lexical-binding: t-*-
+;;; Commentary:
+
+;; rust-common code for both prog-mode and tree-sitter one
+
+;;; Code:
+(defcustom rust-before-save-hook 'rust-before-save-method
+  "Function for formatting before save."
+  :type 'function
+  :group 'rust-mode)
+
+(defcustom rust-after-save-hook 'rust-after-save-method
+  "Default method to handle rustfmt invocation after save."
+  :type 'function
+  :group 'rust-mode)
+
+(provide 'rust-common)
+;;; rust-common.el ends here
